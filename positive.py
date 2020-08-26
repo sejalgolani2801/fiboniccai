23badf49aac2180cb32b664cd586f19f24d009f5
Sejal Golani <69432929+sejalgolani2801@users.noreply.github.com>
2020-08-26 23:16:35 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,17 @@
+lst=[]
+n=int(input("Enter total number of characters to be entered in the list"))
+for i in range(n):
+    a=int(input("enter velue"))
+    lst.append(a)
+print(lst)
+for j in range(n):
+    if lst[j]>0:
+        print (lst[j])
+        
+    
+
+        
+    
+    
+    
+    
